c7ae4ed844709e3a0026bee9540b9603ccd6510c
DDavid <zjdavid.2003@gmail.com>
2020-05-30 18:31:18 +0800

    record_damage.py

--- pcr/plugins/record_damage.py
+++ pcr/plugins/record_damage.py
@@ -5,7 +5,7 @@ class RecordDamage:
     def add_record(self, record: list) -> list:
         # TODO: 传入一个list，将list数据与已有数据比对，如果不同则添加进去
         # TODO: 最后传回新添加的数据（以list形式）
-        # Example List: [['颜夕', '野性狮鹫', '183197'], ['龟仙人', '野性狮鹫', '27891']...]
+        # Example List: [['User1', '野性狮鹫', '183197'], ['User2', '野性狮鹫', '27891']...]
         return list()
 
     def get_record(self) -> list:
