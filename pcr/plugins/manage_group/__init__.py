80a77aa1581fe57df23ab421650f58876e24c003
genres17 <3461372627@qq.com>
2020-06-05 07:51:53 +0800

    Improved the greeting function of new members

--- pcr/plugins/manage_group/__init__.py
+++ pcr/plugins/manage_group/__init__.py
@@ -1,9 +1,13 @@
 from nonebot import on_command, CommandSession
 from nonebot import on_notice, NoticeSession
+import config
 
-group_id = [1104038724, 1108319335]
+#group_id = [1104038724, 1108319335]
 @on_notice('group_increase')
-async def _(session: NoticeSession):
-    if session.event.group_id in group_id:
+async def welcome_new_member(session: NoticeSession):
+    # 群成员增加时自动触发欢迎信息功能
+    if session.event.group_id != config.GROUP_ID and session.event['message_type'] != 'private':
         print(session.event.user_id)
-        await session.send(f"欢迎新人@{session.event.sender['nickname']}加入一起py喵~")
+        await session.send(f"欢迎新的指挥官@{session.event.sender['card']}加入碧蓝焊接指挥部喵~\n"
+                           + config.WELCOME_MESSAGE)
+
