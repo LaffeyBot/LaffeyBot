909964e7bcbd9453c50cb8527b48790122b3f709
zjdavid <zjdavid.2003@gmail.com>
2020-08-07 21:47:34 +0800

    Finished login

--- server_app/auth/auth.py
+++ server_app/auth/auth.py
@@ -1,67 +1,81 @@
 from quart import Blueprint, request, jsonify, g
 import jwt
 import datetime
-from .auth_tools import is_username_exist, verify_sing, get_user_with_username, sign
+from .auth_tools import is_username_exist, get_user_with, sign
 import bcrypt
 import config
-from data.init_database import db_connection_required
-from data.user import User
+from data.model import *
 
 auth_blueprint = Blueprint(
-    "auth",
+    "auth_v1",
     __name__,
-    url_prefix='/auth'
+    url_prefix='/v1/auth'
 )
 
 
 @auth_blueprint.route('/sign_up', methods=['POST'])
-@db_connection_required
 def sign_up():
     if not config.REGISTER_ENABLED:
         return jsonify({"msg": "Register is not enabled."}), 400
     try:
-        qq_id = request.form["qq_id"]  # 要求3字以上好了
-        password = request.form["password"]  # 要求8字以上好了
-        group_id = request.form['group_id']
+        username = request.form["username"]  # 要求3字以上
+        password = request.form["password"]  # 要求8字以上
+        email = request.form.get('email', '')
+        phone = request.form.get('phone', '')
+        nickname = request.form.get('nickname', username)
     except KeyError:
-        return jsonify({"msg": "Missing parameter"}), 400
-    if len(password) < 8:
-        return jsonify({"msg": "Password is too short"}), 400
-    if is_username_exist(qq_id=qq_id, group_id=group_id):
-        return jsonify({"msg": "User Exist"}), 400
-    hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
-    g.db.cursor().execute('UPDATE player_list SET password=%s WHERE qq_id=%s AND group_id=%s',
-                          (hashed_password, qq_id, group_id))
-    g.db.commit()
+        return jsonify({"msg": "Missing parameter", "code": 101}), 400
+    if len(username) < 3 or len(password) < 8:
+        return jsonify({"msg": "Username or password is too short",
+                        "code": 102}), 400
+    if is_username_exist(username):
+        return jsonify({"msg": "User Exists", "code": 103}), 403
+    new_user: Users = Users(group_id=-1,
+                            username=username,
+                            password=bcrypt.hashpw(password, bcrypt.gensalt()),
+                            nickname=nickname,
+                            created_at=datetime.datetime.now(),
+                            role=0,
+                            email=email,
+                            email_verified=False,
+                            phone=phone,
+                            phone_verified=False,
+                            valid_since=datetime.datetime.now()
+                            )
+    db.session.add(new_user)
     return jsonify({
-        "msg": "Success!"
+        "msg": "Successful!",
+        "id": str(new_user.id),
     }), 200
 
 
 @auth_blueprint.route('/login', methods=['POST'])
 def login():
     try:
-        username = request.form["qq_id"]
+        username = request.form.get('username', None)
+        email = request.form.get('email', None)
+        phone = request.form.get('phone', None)
         password: str = request.form["password"]
     except KeyError:
-        return jsonify({"msg": "Username or Password is incorrect"}), 403
-    if is_username_exist(username) is False:
-        return jsonify({"msg": "User does not exist"}), 403
-    user: User = get_user_with_username(username)
+        return jsonify({"mag": "Username or Password is missing", "code": 201}), 403
+    user: Users = get_user_with(username=username)
     if user is None:
-        return jsonify({"mag": "Username or Password is incorrect"}), 403
+        user = get_user_with(email=email)
+    if user is None:
+        user = get_user_with(phone=phone)
+    if user is None:
+        return jsonify({"mag": "User does not exist", "code": 202}), 403
     hash_pwd: str = user.password
     if not bcrypt.checkpw(password.encode(), hash_pwd.encode()):
-        return jsonify({"mag": "Username or Password is incorrect"}), 403
-    user_id: str = str(user["_id"])
+        return jsonify({"msg": "Username or Password is incorrect", "code": 203}), 403
+    user_id: int = user.id
     token = {
         "sub": user_id,
         "iss": config.DOMAIN_NAME,
         "aud": config.FRONTEND_DOMAIN_NAME,
         "iat": int(datetime.datetime.now().timestamp()),
         "remember": True,
-        "type": "login_credential",
-        "level": "master_password"
+        "type": "login_credential"
     }
     signed = sign(token)
     return jsonify({
--- server_app/auth/auth_tools.py
+++ server_app/auth/auth_tools.py
@@ -1,10 +1,10 @@
-from quart import request, jsonify, g
+from quart import request, current_app, jsonify, g
 from functools import wraps
 import jwt
 from data.init_database import db_connection_required
-from data.user import User
-from typing import Optional
 import config
+from data.model import *
+from typing import Optional
 
 
 def login_required(f):
@@ -13,55 +13,58 @@ def login_required(f):
         auth_header = request.headers.get('auth')
         try:
             decode_jwt = jwt.decode(auth_header,
-                                    config.SECRET_KEY,
+                                    current_app.secret_key,
                                     algorithms=['HS256'],
                                     audience=config.DOMAIN_NAME)
         except jwt.exceptions.InvalidTokenError:
-            return jsonify({"msg": "Auth sign does not verify"}), 400
-        user: dict = get_user_with_uid(decode_jwt.get("sub"))
+            return jsonify({"msg": "Auth sign does not verify", "code": 301}), 400
+        user: Users = get_user_with(id_=decode_jwt.get("sub"))
         if user is None:
             return jsonify({"msg": "Can not find user data"}), 403
-        if decode_jwt["iat"] < user["valid_since"].timestamp():  # 若是這個jwt已被撤銷
-            return jsonify({"msg": "This session has been revoked"}), 403
+        if decode_jwt["iat"] < user.valid_since.timestamp():
+            return jsonify({"msg": "This session has been revoked", "code": 302}), 403
         g.user = user
         return f(*args, **kwargs)
 
     return decorated_function
 
 
-@db_connection_required
-def current_user_is_admin():
-    return 'admin' in g.user['group']
+def current_user_is_admin() -> bool:
+    user: Users = g.user
+    if user is None:
+        return False
+    return user.role >= 1
 
 
-@db_connection_required
-def get_user_with_uid(uid: str) -> dict:
-    return g.db.users.find_one({"_id": ObjectId(uid)})
+def current_user_is_owner() -> bool:
+    user: Users = g.user
+    if user is None:
+        return False
+    return user.role >= 2
+
+
+def get_user_with(username: str = None, email: str = None,
+                  phone: str = None, id_: int = None) -> Optional[Users]:
+    if username is not None:
+        return Users.query.filter_by(username=username).first
+    elif email is not None:
+        return Users.query.filter_by(email=email).first
+    elif phone is not None:
+        return Users.query.filter_by(phone=phone).first
+    elif id_ is not None:
+        return Users.query.filter_by(id=id_).first
+    else:
+        return None
 
 
 def sign(json_: dict) -> str:
-    return jwt.encode(json_, current_app.secret_key, algorithm='HS256').decode()
+    return jwt.encode(json_, config.SECRET_KEY, algorithm='HS256').decode()
 
 
 def verify_sing(signed: str) -> dict:
     print(signed)
-    return jwt.decode(signed, current_app.secret_key, algorithms=['HS256'])
+    return jwt.decode(signed, config.SECRET_KEY, algorithms=['HS256'])
 
 
-@db_connection_required
-def is_username_exist(qq_id: str, group_id: str) -> bool:
-    result: int = g.db.cursor().execute('SELECT * FROM player_list '
-                                        'WHERE group_id=%s AND qq_id=%s', (group_id, qq_id))
-    return result != 0
-
-
-@db_connection_required
-def get_user_with_username(qq_id: str, group_id: int) -> Optional[User]:
-    cursor = g.db.cursor()
-    cursor.execute('SELECT (group_id, qq_id, qq_name, role, password, player_name, id) FROM '
-                   'player_list WHERE qq_id=%s AND group_id=%s', (qq_id, group_id))
-    record = cursor.fetchone()
-    if record is None:
-        return None
-    else:
-        return User(record)
+def is_username_exist(username: str) -> bool:
+    return get_user_with(username=username) is not None
