87087a8ec7c7455084833320780b9f4593245831
genres17 <3461372627@qq.com>
2020-09-09 10:18:43 +0800

    add bilibili普通视频信息爬取

---
+++ pcr/plugins/bilibili_info/__init__.py
@@ -0,0 +1,28 @@
+import nonebot
+import re
+from aiocqhttp.event import Event
+from .bilibili_spider import VideoInfo
+bot = nonebot.get_bot()
+
+@bot.on_message()
+async def get_video_info(cq_event: Event):
+    group_id = cq_event.group_id
+    message = cq_event.message
+    urls = re.findall(r'https://www.bilibili.com\S+', str(message))
+    # print('bilibili'+urls)
+    if urls:
+        for url in urls:
+            msg = ''
+            v = VideoInfo(url)
+            result_dic = v.get_video_info()
+            if result_dic['code']==0:
+                msg += f'为指挥官查到{url}的视频简介如下：\n'
+                msg += '视频标题是:'+ result_dic['title']+' up是:'+ result_dic['author']+'\n'
+                msg += '点赞数:'+ str(result_dic['data']['like'])+' 投币数:'+str(result_dic['data']['coin'])+' 收藏数:'+\
+                       str(result_dic['data']['favorite'])+' 分享数:'+str(result_dic['data']['share'])+'\n'
+                msg += '当前播放数为:'+str(result_dic['data']['view'])+' 弹幕数:'+str(result_dic['data']['danmaku'])+'\n'
+                msg += '视频简介:\n'+result_dic['self_introduce']
+                await bot.send_msg(group_id=group_id,message=msg)
+            else:
+                await bot.send_msg(group_id=group_id,message=result_dic['message'])
+
